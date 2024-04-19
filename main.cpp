4d240f4505c4ee5b8e6c5abcf7bfa023c5a830e3
yusufoner77 <yusufoner7@gmail.com>
2024-04-19 17:55:33 -0500

    finished base code

--- main.cpp
+++ main.cpp
@@ -20,13 +20,13 @@ class Dish {
     }
 };
 
-class Stack {
+class DishStack {
   private:
     Dish stack[50];
     int index;
 
   public:
-    Stack() {
+    DishStack() {
       index = -1;
     }
 
@@ -71,5 +71,48 @@ class Stack {
 };
 
 int main() {
+  DishStack stack;
+  Dish one_dish = Dish("A dish with one fish pattern on it");
+  Dish two_dish = Dish("A dish with two fish patterns on it");
+  Dish red_dish = Dish("A dish with a red fish pattern on it");
+  Dish blue_dish = Dish("A dish with a blue fish pattern on it");
 
+  // The variable stack_size should equal zero.
+  int stack_size = stack.size();
+  cout << stack_size << endl;
+
+  stack.push(one_dish);
+  stack.push(two_dish);
+  stack.push(red_dish);
+  stack.push(blue_dish);
+
+  // The variable size_after_pushes should equal four.
+  int size_after_pushes = stack.size();
+  cout << size_after_pushes << endl;
+
+  // The variable peeked_dish should have a description of "A dish with a blue fish pattern on it"
+  Dish peeked_dish = stack.peek();
+  cout << peeked_dish.get_description() << endl;
+
+  // The variable popped_dish should have a description of "A dish with a blue fish pattern on it"
+  Dish popped_dish = stack.pop();
+  cout << popped_dish.get_description() << endl;
+
+  // The variable another_popped_dish should have a description of "A dish with a red fish pattern on it"
+  Dish another_popped_dish = stack.pop();
+  cout << another_popped_dish.get_description() << endl;
+
+  // The variable final_size should equal two.
+  int final_size = stack.size();
+  cout << final_size << endl;
+
+  // This should trigger an error message that the stack is full
+  for (int i = 0; i < 49; i ++) {
+    stack.push(one_dish);
+  }
+
+  // This should trigger an error message that you can't pop an empty stack
+  for (int i = 0; i < 51; i++) {
+    stack.pop();
+  }
 }
